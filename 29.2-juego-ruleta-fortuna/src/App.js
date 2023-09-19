7f46e1c8cded6d4a2b9f684ae998bd8ec3d59ea4
David Rodríguez <jacodaro@hotmail.com>
2023-09-19 19:20:36 +0200

    Lección 29 Juego Ruleta fortuna

---
+++ 29.2-juego-ruleta-fortuna/src/App.js
@@ -0,0 +1,131 @@
+import React, { useRef, useState } from 'react'
+
+function App() {
+  const [ancho, setAncho] = useState(1)
+  const [monedas, setMonedas] = useState(1)
+  const [tiradas, setTiradas] = useState(1)
+  const [premio, setPremio] = useState("Empiezas con una moneda y un ticket de tirada. Selecciona la potencia con la que quieres lanzar la ruleta y ganar monedas.")
+  const [rotacion, setRotacion] = useState(0)
+  const [situacion, setSituacion] = useState(0)
+  const xRoja = './assets/x_roja.png'
+
+  const barraRef = useRef(null)
+
+  const lanzar = () => {
+    if (barraRef.current) {
+      setTiradas(-1)
+      barraRef.current.classList.toggle('paradaBarraPotencia');
+      const potenciaSeleccionada = barraRef.current.getBoundingClientRect().width;
+      setAncho(potenciaSeleccionada)
+      girarRuleta()
+      setSituacion(1)
+    }
+  }
+  const girarRuleta = () => {
+    const rotacionAleatoria = Math.floor(Math.random() * 210) + 340;
+    setPremio("Suerte...");
+    setRotacion(rotacion + ancho + rotacionAleatoria);
+  }
+
+  const posicionTirada = () => {
+    setSituacion(0)
+    const gradosRotacion = (rotacion % 360 + 360) % 360;
+
+    switch (true) {
+      case gradosRotacion >= 0 && gradosRotacion <= 44 || gradosRotacion >= 180 && gradosRotacion <= 224:
+        setPremio("Casilla de muerte. Has perdido!")
+        setMonedas(0)
+        setSituacion(2)
+        break;
+      case (gradosRotacion >= 45 && gradosRotacion <= 90):
+        setPremio(`Ganas 1 y sumas ${(monedas + 1)} monedas.`)
+        setMonedas(monedas + 1)
+        setTiradas(tiradas + 1)
+        break;
+      case (gradosRotacion >= 91 && gradosRotacion <= 135):
+        setPremio(`Doblas x2 y sumas ${(monedas * 2)} monedas.`)
+        setMonedas(monedas * 2)
+        setSituacion(2)
+        break;
+      case (gradosRotacion >= 136 && gradosRotacion <= 179):
+        setPremio(`Ganas 8 y sumas ${(monedas + 8)} monedas.`)
+        setMonedas(monedas + 8)
+        setTiradas(tiradas + 1)
+        break;
+      case (gradosRotacion >= 225 && gradosRotacion <= 269):
+        setPremio(`Ganas 5 y sumas ${(monedas + 5)} monedas.`)
+        setMonedas(monedas + 5)
+        setTiradas(tiradas + 1)
+        break;
+      case (gradosRotacion >= 270 && gradosRotacion <= 314):
+        setPremio(`Multiplicas x3 y sumas ${(monedas * 3)} monedas.`)
+        setMonedas(monedas * 3)
+        setSituacion(2)
+        break;
+      case (gradosRotacion >= 315 && gradosRotacion <= 359):
+        setPremio(`Ganas 2 y sumas ${(monedas + 2)} monedas.`)
+        setMonedas(monedas + 2)
+        setTiradas(tiradas + 1)
+        break;
+      default:
+        setPremio('Default')
+        break
+    }
+  }
+
+  const reiniciar = () => {
+    setMonedas(1)
+    setTiradas(1)
+    setPremio("Empiezas con una moneda y un ticket de tirada. Selecciona la potencia con la que quieres lanzar la ruleta y ganar monedas.")
+    setRotacion(0)
+    setSituacion(0)
+  }
+  return (
+    <>
+      <div className='monedas'>
+        {monedas === 0
+          ? <img src={xRoja} alt='X roja' />
+          /* array.from() es una función de JS que permite convertir un elemento en un array */
+          : Array.from({ length: monedas }, (_, index) =>
+            <img key={index} src='./assets/moneda.png' alt={`Moneda`} />
+          )
+        }
+      </div>
+      <div className='tiradas'>
+        {
+          Array.from({ length: tiradas }, (_, index) =>
+            <img key={index} src='./assets/ticket.png' alt={`Ticket`} />)
+        }
+
+      </div>
+      <div className='tablero'>
+        <div className='ruleta' style={{
+          backgroundImage: `url('./assets/ruleta.png')`,
+          transform: `rotate(${rotacion}deg)`,
+          transition: "transform 6s cubic-bezier(0.2, 0.8, 0.7, 0.99)",
+        }}
+          onTransitionEnd={posicionTirada}>
+        </div>
+        <div className='premio'>
+          <h3>{premio}</h3>
+        </div>
+        <div className='lanzador'>
+          {situacion === 0 &&
+            <div className='indicador'>
+              <div className='indicadorPotencia' ref={barraRef}></div>
+            </div>
+          }
+        </div>
+        <div className='barraInferior'>
+          {tiradas > 0 && <button className='lanzar' onClick={lanzar}>Lanzar</button>}
+          {situacion === 2 && <h1>No te quedan más tiradas. Has ganado {monedas} monedas.</h1>}
+        </div>
+        <div className='marcador'>
+          <img src='./assets/central.png' alt='Logo central' />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
